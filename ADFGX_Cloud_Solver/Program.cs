4b4d2530ec73d830118d99b90c7ae24cfef58566
waterkh <pjclark@email.sc.edu>
2016-10-26 15:10:40 -0400

    Mac/ Linux Build Capability

--- ADFGX_Cloud_Solver/Program.cs
+++ ADFGX_Cloud_Solver/Program.cs
@@ -14,9 +14,6 @@ namespace ADFGX_Cloud_Solver
         [STAThread]
         static void Main()
         {
-            if (Environment.OSVersion.Version.Major >= 6)
-                SetProcessDPIAware();
-
             if (!System.IO.File.Exists("ADFGX_Cloud_Solver.exe.config"))
             {
                 System.IO.File.WriteAllText("ADFGX_Cloud_Solver.exe.config", Properties.Resources.ADFGX_Cloud_Solver_exe);
@@ -26,8 +23,5 @@ namespace ADFGX_Cloud_Solver
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
-
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        private static extern bool SetProcessDPIAware();
     }
 }
